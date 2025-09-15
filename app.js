dca0c08deec98d9f56eb1052316eaf5f11048878
gurbanliaisha <aisha.qurbanli@gmail.com>
2025-09-15 18:48:12 +0400

    Adding custom error handler middleware

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config(); //this config method write all the variables in the env file to the process.env and then we can call each variable in anywhere we want
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.static("./public"));
 app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
