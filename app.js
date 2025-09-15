3e55f9ed61c341c26780a4bf0395e65d1ad702c9
gurbanliaisha <aisha.qurbanli@gmail.com>
2025-09-15 21:14:36 +0400

    Port value setup

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use("/api/v1/tasks", tasks);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.Port || 3000;
 
 const start = async () => {
   try {
